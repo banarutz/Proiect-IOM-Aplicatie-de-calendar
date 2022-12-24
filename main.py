bd56d4377c62b0e2a81b309d44217498c46d2691
banarutz <65508171+banarutz@users.noreply.github.com>
2022-12-24 17:27:20 +0200

    Sebi (#3)
    * PR test
    
    * main
    
    * Insert just one event - working.
    
    Co-authored-by: Bănaru M. Sebastian <mircea.banaru@stud.etti.upb.ro>
    Co-authored-by: Harth01 <81443376+Harth01@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from imports import *
+######################################
+# Date is formatted as month/day/year#
+######################################
 
+global EventData
 
 root = Tk()
 root.title('IOM project calendar')
@@ -8,6 +12,44 @@ root.geometry('600x400')
 
 cal = Calendar (root, selectmode = 'day', year = 2023, month = 1, day = 15)
 cal.pack()
+
+
+def InsertEvent (): # Momentan insereaza un singur eveniment 
+    # global EventInfo, data
+    data = cal.get_date()   
+    event = InsertEventText.get(1.0, tk.END) # EVENTUL ARE UN \N DUPA EL, NU STIU DE CE? TREBUIE SCOS
+    InsertedData = [data, event]
+    print(InsertedData)
+    # my_label.config (InsertedData)
+    with open('EventData.csv', 'w') as file:
+        writer = csv.writer(file, lineterminator= '\n')
+        writer.writerow([data, event])
+    RemoveTextBoxAndButton()
+    # Functie care scoate text box-ul si anunta ca evenimentul a fost introdus cu succes!
+
+
+def ShowInsertEventTextBox ():
+    global InsertIntoCSVButton, InsertEventLabel, data, InsertEventText
+    InsertEventLabel = Label (root, text = 'Introduceți evenimentul:')
+    InsertEventLabel.place (x = 125, y = 235)
+    InsertEventText = Text(root, height = 5, width = 25)
+    InsertEventText.place( x = 125, y = 260)
+    InsertIntoCSVButton = Button (root, text= 'Introduceti textul', command = InsertEvent)
+    InsertIntoCSVButton.place (x = 125, y = 350)
+
+def RemoveTextBoxAndButton():
+    InsertIntoCSVButton.place_forget()
+    InsertEventText.place_forget()
+    InsertEventLabel.place_forget()
+
+def speech_event ():
+    RemoveTextBoxAndButton()
+    pass
+
+InsertEventButton = Button (root, text= 'Insereaza eveniment', command = ShowInsertEventTextBox).place (x = 125, y = 200)
+SpeechEventButton = Button (root, text= 'Rostește eveniment', command = speech_event).place (x = 350, y = 200)
+
+# myButton.pack(pady = 20)
 
 def grab_date ():
     my_label.config (text = "Today's date is: " + cal.get_date())
@@ -18,4 +60,4 @@ myButton.pack(pady = 20)
 my_label = Label (root, text='')
 my_label.pack (pady = 20)
 
-root.mainloop()
+root.mainloop() 
